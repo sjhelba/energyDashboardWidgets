e728ec550403773d06c59c14aeae293db4c2a569
Sara Helba <sara.helba@tekworx.us>
2018-04-27 16:40:59 -0400

    grid working

--- 5heatMap/browser_heatMap.js
+++ 5heatMap/browser_heatMap.js
@@ -35,15 +35,55 @@ function defineFuncForTabSpacing () {
 		const selectionForCheck = outerWidgetEl.selectAll(elementsToReset)
 		if (!selectionForCheck.empty()) selectionForCheck.remove();
 	};
-	const getMonthlyDataForYear = (hourlyData, year, tempRanges) => {
-		console.log('hourlyData: ', hourlyData)
-		let hrsPerTempRangePerMonth = [];
+	const getIndexOfBinForTemp = (temp, binArr) => binArr.findIndex(bin => temp >= bin.min && temp <= bin.max);
+
+	const getMonthlyDataForYear = (hourlyData, year, tempRanges, effRange, formatKwTrFunc) => {
+		const makeAMonthObj = month => {
+			const obj = {
+				thisMonth: month,
+				totalHoursForMonth: 0,
+				// create arr of {} (one for each tempBin) for each month's monthObj.tempBinsForMonth
+				tempBinsForMonth: tempRanges.map(range => {
+					return {
+							totalHoursInBin: 0,
+							totalEffInBin: 0,
+							avgEffInBin: 0,
+							opacity: 0,
+							colorScaleVal: 0,
+							thisTempBin: range
+					};
+				})
+			};
+			return obj;
+		}
+		const hrsPerTempRangePerMonth = [];
 		months.forEach(month => {
+			let monthObj = makeAMonthObj(month);
+			// if there is data for that month in hourlyData, add it to monthObj
+			if (hourlyData[year][month]){
+				hourlyData[year][month].hoursArr.forEach(hour => {
+					monthObj.totalHoursForMonth++;
+					monthObj.tempBinsForMonth[getIndexOfBinForTemp(hour.temp, tempRanges)].totalHoursInBin++;
+					monthObj.tempBinsForMonth[getIndexOfBinForTemp(hour.temp, tempRanges)].totalEffInBin += hour.eff;
+				});
+			}
 
-		})
+			// monthObj.totalHoursForMonth = monthObj.tempBinsForMonth.reduce(((accum, curr) => accum + curr.totalHoursInBin), 0)
+			
+			// add month's monthObj with data to hrsPerTempRangePerMonth
+			hrsPerTempRangePerMonth.push(monthObj);
+		});
 
-	}
-
+		// calculate avg eff, opacity, and colorScaleVal for each bin in each month
+		hrsPerTempRangePerMonth.forEach(month => {
+			month.tempBinsForMonth.forEach(bin => {
+				bin.avgEffInBin = bin.totalEffInBin / bin.totalHoursInBin ? +formatKwTrFunc(bin.totalEffInBin / bin.totalHoursInBin) : 0;
+				bin.opacity = bin.totalHoursInBin / month.totalHoursForMonth || 0;
+				bin.colorScaleVal = bin.avgEffInBin < effRange[0] ? effRange[0] : (bin.avgEffInBin > effRange[1] ? effRange[1] : bin.avgEffInBin);
+			});
+		});
+		return hrsPerTempRangePerMonth;
+	};
 
 
 
@@ -344,8 +384,10 @@ function defineFuncForTabSpacing () {
 					data.hourlyData[hourlyDatum.year] = {};
 					data.availableYears.unshift(hourlyDatum.year)
 				}
-				if (!data.hourlyData[hourlyDatum.year][hourlyDatum.month]) data.hourlyData[hourlyDatum.year][hourlyDatum.month] = {};
-				data.hourlyData[hourlyDatum.year][hourlyDatum.month][hourlyDatum.hour] = {eff: hourlyDatum.value, temp: undefined};
+				if (!data.hourlyData[hourlyDatum.year][hourlyDatum.month]) {
+					data.hourlyData[hourlyDatum.year][hourlyDatum.month] = {monthObj: {}, hoursArr: []};
+				}
+				data.hourlyData[hourlyDatum.year][hourlyDatum.month].monthObj[hourlyDatum.dateHr] = {eff: hourlyDatum.value, temp: undefined};
 			});
 			tempHistory.forEach(hourlyDatum => {
 				if (!data.hourlyData[hourlyDatum.year]) {
@@ -353,17 +395,27 @@ function defineFuncForTabSpacing () {
 					data.hourlyData[hourlyDatum.year] = {};
 					data.availableYears.unshift(hourlyDatum.year)
 				}
-				if (!data.hourlyData[hourlyDatum.year][hourlyDatum.month]) data.hourlyData[hourlyDatum.year][hourlyDatum.month] = {};
-				if (!data.hourlyData[hourlyDatum.year][hourlyDatum.month][hourlyDatum.hour]) {
-					data.hourlyData[hourlyDatum.year][hourlyDatum.month][hourlyDatum.hour] = {eff: undefined, temp: hourlyDatum.value};
+				if (!data.hourlyData[hourlyDatum.year][hourlyDatum.month]) {
+					data.hourlyData[hourlyDatum.year][hourlyDatum.month] = {monthObj: {}, hoursArr: []};
+				}
+				if (!data.hourlyData[hourlyDatum.year][hourlyDatum.month].monthObj[hourlyDatum.dateHr]) {
+					data.hourlyData[hourlyDatum.year][hourlyDatum.month].monthObj[hourlyDatum.dateHr] = {eff: undefined, temp: hourlyDatum.value};
 				} else {
-					data.hourlyData[hourlyDatum.year][hourlyDatum.month][hourlyDatum.hour].temp = hourlyDatum.value;
+					data.hourlyData[hourlyDatum.year][hourlyDatum.month].monthObj[hourlyDatum.dateHr].temp = hourlyDatum.value;
 				}
 			});
 
-		};
+
 		if (addedTempHistoryYr) data.availableYears.sort((a,b) => a-b);
 
+		//convert collected hours to array format within overall data obj
+		data.availableYears.forEach(yearKey => {
+			const monthKeys = Object.keys(data.hourlyData[yearKey]);
+			monthKeys.forEach(monthKey => {
+				data.hourlyData[yearKey][monthKey].hoursArr = d3.values(data.hourlyData[yearKey][monthKey].monthObj);
+			})
+		})
+	};
 
 		// CALCULATED DEFS //
 		const calculateDefs = () => {
@@ -379,11 +431,14 @@ function defineFuncForTabSpacing () {
 			data.yAxisTextAreaWidth = getTextHeight(data.yAxisTitleFont) + data.paddingRightOfYAxisTitle + getTextWidth(`-${data.formatTemp(-888.888)}-${data.formatTemp(-888.888)}`, data.yAxisTicksTextFont) + data.paddingRightOfYAxisTicks;
 			data.legendWidth = getTextHeight(data.legendTitleFont) + data.paddingRightOfLegendTitle + data.legendBarWidth + data.paddingRightOfLegendBar + getTextWidth(`>${data.formatKwTr(8.8888)}`, data.legendTicksTextFont);
 			data.gridWidth = data.graphicWidth - (data.yAxisTextAreaWidth + data.legendWidth + data.paddingRightOfGrid)
+			data.cellWidth = data.gridWidth / 12;
 
 			//heights
 			data.headerAreaHeight = data.dropdownGroupHeight + data.paddingBelowDropdown;
 			data.gridHeight = data.graphicHeight - (data.headerAreaHeight + data.paddingBelowGrid + getTextHeight(data.xAxisTicksTextFont));
+			data.cellHeight = data.gridHeight / 12;
 
+			// yAxis Bins
 			const getSmallestNumOfPrecision = decimals => {
 				if (!decimals) return 1;
 				let decimalNums = '0'.repeat(decimals - 1);
@@ -393,20 +448,26 @@ function defineFuncForTabSpacing () {
 
 			const smallestNumOfPrecision = getSmallestNumOfPrecision(data.tempPrecision)
 			data.tempBins = [];
-			data.tempBins.push({min: Number.NEGATIVE_INFINITY, max: data.minTempCategory - 0.0000001, display: '<' + data.formatTemp(data.minTempCategory)})
-			data.tempIntervals = (data.maxTempCategory - data.minTempCategory) / 10;	// to get 12 bins
+			data.tempBins.push({min: Number.NEGATIVE_INFINITY, max: data.minTempCategory - 0.00000000000000000000000000000001, display: '<' + data.formatTemp(data.minTempCategory)})
+			data.tempBinsInterval = (data.maxTempCategory - data.minTempCategory) / 10;	// to get 12 bins
 			for (let i = 0; i <= 9; i++) {
 				let bin = {};
-				bin.min = data.minTempCategory + (data.tempIntervals * i);
-				bin.max = (bin.min + data.tempIntervals) - 0.0000001;
-				bin.display = data.formatTemp(bin.min) + '-' + data.formatTemp(((bin.min + data.tempIntervals) - smallestNumOfPrecision));
+				bin.min = data.minTempCategory + (data.tempBinsInterval * i);
+				bin.max = (bin.min + data.tempBinsInterval) - 0.00000000000000000000000000000000000001;
+				bin.display = data.formatTemp(bin.min) + '-' + data.formatTemp(((bin.min + data.tempBinsInterval) - smallestNumOfPrecision));
 				data.tempBins.push(bin)
 			}
 			data.tempBins.push({min: data.maxTempCategory, max: Number.POSITIVE_INFINITY, display: '≥' + data.formatTemp(data.maxTempCategory)})
-
-			getMonthlyDataForYear(data.hourlyData, widget.dropdownYearSelected)
 			console.log('tempBins: ', data.tempBins)
-			
+
+			// legend range
+			data.legendRange = [data.minKwTrCategory, data.maxKwTrCategory]
+
+			// get dataForYear
+			data.dataForYear = getMonthlyDataForYear(data.hourlyData, widget.dropdownYearSelected, data.tempBins, data.legendRange, data.formatKwTr);
+			console.log('dataForYear: ', data.dataForYear)
+
+			// return data
 			return data;
 		};
 
@@ -489,21 +550,82 @@ function defineFuncForTabSpacing () {
 
 
 
+		// ********************************************* GENERATORS AND SCALES ******************************************************* //
+		const xScale = d3.scaleBand()
+			.domain(months)
+			.range([0, data.gridWidth]);
+
+		const xAxisGenerator = d3.axisBottom()
+			.scale(xScale)
+			.tickFormat(d => d)
+			.tickSize(0)
+
+		const yScale = d3.scaleBand()
+			.domain(data.tempBins.map(bin => bin.display))
+			.range([data.gridHeight, 0])
+		
+		const yAxisGenerator = d3.axisLeft()
+			.scale(yScale)
+			.tickFormat(d => d)
+			.tickSize(0)
+
+		const colorScale = d3.scaleLinear()
+			.domain([data.minKwTrCategory, data.maxKwTrCategory])
+			.range([data.midKwTrColor, data.minKwTrColor])
 
 		// ********************************************* CHART ******************************************************* //
 		const gridGroup = chartGroup.append('g')
 			.attr('class', 'gridGroup')
 			.attr('transform', `translate(${data.yAxisTextAreaWidth}, 0)`)
 	
-		gridGroup.append('rect')		// TODO: remove
+		// chart background rect
+		gridGroup.append('rect')
 			.attr('height', data.gridHeight)
 			.attr('width', data.gridWidth)
-			.attr('stroke', 'black')
-			.attr('fill', 'none')
+			.attr('stroke', data.gridStrokeColor)
+			.attr('fill', data.gridFillColor)
+			.style('stroke-weight', '0.5pt')
 			
+		const monthlyGroups = gridGroup.selectAll('.monthlyGroup')
+			.data(data.dataForYear)
+			.enter().append('g')
+				.attr('class', 'monthlyGroup')
+				.attr('transform', d => `translate(${xScale(d.thisMonth)}, 0)`)
+
+		monthlyGroups.selectAll('.cell')
+			.data(d => d.tempBinsForMonth)
+			.enter().append('rect')
+				.attr('class', 'cell')
+				.attr('width', data.cellWidth)
+				.attr('height', data.cellHeight)
+				.attr('y', d => yScale(d.thisTempBin.display))
+				.attr('fill', d => colorScale(d.colorScaleVal))
+				.style('opacity', 1) //d => d.opacity < 0.2 && d.opacity !== 0 ? 0.2 : d.opacity)
 
 
 
+		// x axis
+		const xAxis = gridGroup.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', `translate(0, ${data.gridHeight})`)
+			.call(xAxisGenerator)
+		xAxis.selectAll('text')
+			.style('font', data.xAxisTicksTextFont)
+			.attr('fill', data.xAxisTicksTextColor)
+		xAxis.selectAll('path')
+			.attr('stroke', 'none')
+
+		
+		// y axis
+		const yAxis = gridGroup.append('g')
+			.attr('class', 'y axis')
+			.call(yAxisGenerator)
+		yAxis.selectAll('text')
+				.style('text-anchor', 'end')
+				.style('font', data.yAxisTicksTextFont)
+				.attr('fill', data.yAxisTicksTextColor)
+		yAxis.selectAll('path')
+			.attr('stroke', 'none')
 
 
 
--- 5heatMap/fakeData_heatMap.js
+++ 5heatMap/fakeData_heatMap.js
@@ -2,7 +2,7 @@ tempHistory = [];
 effHistory = [];
 
 const getRandomInt = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
-const tempMin = 28;
+const tempMin = 25;
 const tempMax = 85;
 
 const effMin = 200;
@@ -18,6 +18,7 @@ let jsThisYear = jsToday.getFullYear();
 let jsLastYear = jsThisYear - 1;
 let jsThisMonthIndex = jsToday.getMonth();
 let jsThisMonth = jsMonths[jsThisMonthIndex];
+let jsThisDate = jsToday.getDate()
 let jsThisHour = jsToday.getHours();
 
 const monthIndicesInYears = {
@@ -28,24 +29,26 @@ const monthIndicesInYears = {
 
 for (let year = jsThisYear; year >= jsLastYear; year--) {
   for (let monthIndex = monthIndicesInYears[year]; monthIndex >= 0; monthIndex--) {
-    for (let hour = year === jsThisYear && monthIndex === jsThisMonthIndex ? jsThisHour : 23; hour >= 0; hour--) {
+    for (let dayOfMonth = year === jsThisYear && monthIndex === jsThisMonthIndex ? jsThisDate : 30; dayOfMonth > 0; dayOfMonth--) {
+      for (let hour = year === jsThisYear && monthIndex === jsThisMonthIndex && dayOfMonth === jsThisDate ? jsThisHour : 23; hour >= 0; hour--) {
 
-      let tempEntry = {
-        year: year,
-        month: jsMonths[monthIndex],
-        hour: hour,
-        value: getRandomTemp()
-      };
-      let effEntry = {
-        year: year,
-        month: jsMonths[monthIndex],
-        hour: hour,
-        value: getRandomEff()
-      };
+        let tempEntry = {
+          year: year,
+          month: jsMonths[monthIndex],
+          dateHr: [dayOfMonth, hour],
+          value: getRandomTemp()
+        };
+        let effEntry = {
+          year: year,
+          month: jsMonths[monthIndex],
+          dateHr: [dayOfMonth, hour],
+          value: getRandomEff()
+        };
 
-      // newest entries later in indices
-      tempHistory.unshift(tempEntry);
-      effHistory.unshift(effEntry);
+        // newest entries later in indices
+        tempHistory.unshift(tempEntry);
+        effHistory.unshift(effEntry);
+      }
     }
   }
 }
