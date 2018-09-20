431a174c8d0a3e5f6eb3af022cb2f95c9d2b2a3b
Sara Helba <29603156+sjhelba@users.noreply.github.com>
2018-09-20 16:45:09 -0400

    updates

---
+++ classes/TekWorxColors.js
@@ -0,0 +1,13 @@
+class TekWorxColors {}
+
+TekWorxColors.orange = '#Ecb550';
+TekWorxColors.green = '#22b573';
+TekWorxColors.darkGray = '#404040';
+TekWorxColors.lightGray = '#c0c0c0';
+TekWorxColors.red = '#d53d3b';
+TekWorxColors.blue = '#3Ea9f5';
+TekWorxColors.lightBlue = 'rgb(105,202,210)'
+TekWorxColors.darkGrayBlue = '#425867';
+TekWorxColors.deepBlue = '#060084'
+TekWorxColors.teal = '#1dc1e4';
+TekWorxColors.offWhite = '#d5d6d4'
